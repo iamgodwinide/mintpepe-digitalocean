609541c6af0d6de4bbebf5d92fd854bea44814b7
Godwin Idemudia <godwinide@gmail.com>
2023-05-29 00:07:00 +0100

    new

--- src/components/MintButton/Mint.jsx
+++ src/components/MintButton/Mint.jsx
@@ -4,7 +4,7 @@ import newcontract from './Newcontract.json';
 import { useAlert } from 'react-alert';
 import './style.css'
 
-const newcontractAddress = "0x88bE91c05ca35fB99ad8C9f3B666273bB9613B0d";
+const newcontractAddress = "0x6e3E5D6B2E9ec13745dFd884a083Ca50C1854dED";
 
 const Mint = ({ accounts, setAccounts }) => {
     const [price, setPrice] = useState('0.01')
